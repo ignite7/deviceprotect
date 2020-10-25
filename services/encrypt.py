f84fdecc82484f27e2240baec9292feeb9d5e074
ignite7 <ignite21@protonmail.com>
2020-10-25 02:15:04 +0100

    fixed encrypt folders

--- services/encrypt.py
+++ services/encrypt.py
@@ -6,7 +6,7 @@ Encrypt service.
 from cryptography.fernet import Fernet
 
 # Utilities
-from .utils import create_key, output, find_files
+from .utils import create_key, output
 import os
 
 
@@ -29,17 +29,27 @@ class EncryptService:
         """
         Encrypt files.
         """
-        
-        patterns = find_files(self.kwargs)
 
-        for pattern in patterns:
-            with open(pattern, 'rb') as raw_file:
+        if os.path.isfile(self.kwargs):
+            with open(self.kwargs, 'rb') as raw_file:
                 file_data = raw_file.read()
 
             encrypt_data = self.fernet.encrypt(file_data)
 
-            with open(pattern, 'wb') as raw_file:
+            with open(self.kwargs, 'wb') as raw_file:
                 raw_file.write(encrypt_data)
 
-        return output(self.key, self.kwargs)
+        elif os.path.isdir(self.kwargs):
+            for dirs_path, dirs, files in os.walk(self.kwargs):
+                for name in files:
+                    rel_path = os.path.join(dirs_path, name)
 
+                    with open(rel_path, 'rb') as raw_file:
+                        file_data = raw_file.read()
+
+                    encrypt_data = self.fernet.encrypt(file_data)
+
+                    with open(rel_path, 'wb') as raw_file:
+                        raw_file.write(encrypt_data)
+
+        return output(self.key, self.kwargs)
--- services/utils.py
+++ services/utils.py
@@ -6,6 +6,7 @@ Utils service.
 from cryptography.fernet import Fernet
 
 # Utilities
+import os
 import sys
 import getpass
 
@@ -22,32 +23,6 @@ def create_key():
     return Fernet.generate_key()
 
 
-def find_files(user_path):
-    """
-    Iterate for the path the
-    user gave and find all
-    files.
-    """
-
-    if os.path.isfile(user_path):
-        patterns = [user_path]
-
-    elif os.path.isdir(user_path):
-        accumulate = []
-        patterns = user_path.rstrip('/') + \
-            '/{}'.format(next(os.walk(user_path))[1])
-
-        for pattern in patterns:
-            if os.path.isfile(pattern):
-                accumulate.append(pattern)
-
-            else:
-                #TODO: Improve idea
-                pass
-
-    return patterns or accumulate
-
-
 def output(key, file_path):
     """
     Shows the output to the user.
@@ -55,10 +30,10 @@ def output(key, file_path):
 
     if sys.platform.startswith('linux'):
         user_path = '/home/{}/deviceprotect.txt'.format(USER)
- 
+
     elif sys.platform.startswith('win32'):
-        user_path = 'C\\Users\\{}/deviceprotect.txt'.format(USER)
-  
+        user_path = 'C:\\User\\{}/deviceprotect.txt'.format(USER)
+
     message = (
         'IMPORTANT DATA.\nKEY: {}\nPATH: {}\n'
     ).format(key, file_path)
